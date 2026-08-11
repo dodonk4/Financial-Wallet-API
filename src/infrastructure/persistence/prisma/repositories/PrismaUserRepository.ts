d2c20752cecda80592e984923a82d013bb69bf16
dodonk4 <ismael.madarieta@hotmail.com>
2026-08-10 21:47:40 -0300

    feat: adding create method to PrismaUserRepository

--- src/infrastructure/persistence/prisma/repositories/PrismaUserRepository.ts
+++ src/infrastructure/persistence/prisma/repositories/PrismaUserRepository.ts
@@ -3,7 +3,7 @@ import { IUserRepository } from "../../../../application/ports/output/IUserRepos
 import { User } from "../../../../domain/entities/User.ts";
 
 export class PrismaUserRepository implements IUserRepository {
-  constructor(private readonly prisma: PrismaClient | Prisma.TransactionClient) {}
+  constructor(private readonly prisma: PrismaClient | Prisma.TransactionClient) { }
 
   async existsByEmail(email: string): Promise<boolean> {
 
@@ -16,13 +16,13 @@ export class PrismaUserRepository implements IUserRepository {
     return user !== null;
   }
 
-  async existsByDocument(documentType: string, documentNumber: string): Promise<boolean> {
+  async existsByDocument(identifierType: string, identifierNumber: number): Promise<boolean> {
 
     const user = await this.prisma.user.findUnique({
       where: {
         identifierType_identifierNumber: {
-          identifierType: documentType,
-          identifierNumber: documentNumber,
+          identifierType: identifierType,
+          identifierNumber: identifierNumber,
         },
       }
     })
@@ -32,7 +32,30 @@ export class PrismaUserRepository implements IUserRepository {
   }
 
   async create(user: User): Promise<User> {
+    const createdUser = await this.prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        passwordHash: user.passwordHash,
+        identifierType: user.identifierType,
+        identifierNumber: user.identifierNumber,
+        role: user.role,
+      },
+    });
 
+    const newCreatedUser = User.create({
+      id: createdUser.id,
+      email: createdUser.email,
+      passwordHash: createdUser.passwordHash,
+      firstName: createdUser.firstName,
+      lastName: createdUser.lastName,
+      identifierType: createdUser.identifierType,
+      identifierNumber: createdUser.identifierNumber,
+    })
+
+    return newCreatedUser;
   }
 
   async findById(id: string): Promise<User | null> {
