80d7f189590eef9ab6cccb27018016f0916c0566
dodonk4 <ismael.madarieta@hotmail.com>
2026-08-12 17:58:36 -0300

    feat: adding create to PrismaAccountRepository

--- src/infrastructure/persistence/prisma/repositories/PrismaAccountRepository.ts
+++ src/infrastructure/persistence/prisma/repositories/PrismaAccountRepository.ts
@@ -8,7 +8,23 @@ export class PrismaAccountRepository implements IAccountRepository {
   constructor(private readonly prisma: PrismaClient | Prisma.TransactionClient) { }
 
   async create(account: Account): Promise<Account> {
+    const prismaCreatedAccount = await this.prisma.account.create({
+      data: {
+        id: account.id,
+        userId: account.userId,
+        balance: account.balance,
+        currency: account.currency,
+        status: account.status,
+      },
+    });
 
+    const createdAccount = Account.create({
+      id: prismaCreatedAccount.id,
+      userId: prismaCreatedAccount.userId,
+      currency: prismaCreatedAccount.currency
+    })
+
+    return createdAccount;
   }
 
   // resto de métodos...
