3a0b261e19ea0bcfda2d6e07e24a95885a151aa1
dodonk4 <ismaelmadarieta49@gmail.com>
2026-08-17 15:41:16 -0300

    feat: completing LogunUseCase

--- src/application/use-cases/auth/login/LoginUseCase.ts
+++ src/application/use-cases/auth/login/LoginUseCase.ts
@@ -1,3 +1,6 @@
+import { randomUUID } from "node:crypto";
+import { RefreshToken } from "../../../../domain/entities/RefreshToken.ts";
+import { User } from "../../../../domain/entities/User.ts";
 import { ForbiddenError } from "../../../../domain/errors/ForbiddenError.ts";
 import { InvalidCredentialsError } from "../../../../domain/errors/InvalidCredentialsError.ts";
 import { IEventPublisher } from "../../../ports/output/IEventPublisher.ts";
@@ -7,6 +10,7 @@ import { IUnitOfWork } from "../../../ports/output/IUnitOfWork.ts";
 import { IUserRepository } from "../../../ports/output/IUserRepository.ts";
 import { LoginRequestDTO } from "./LoginRequestDTO.ts";
 import { LoginResponseDTO } from "./LoginResponseDTO.ts";
+import { ITokenHasher } from "../../../ports/output/ITokenHasher.ts";
 
 export class LoginUseCase {
     constructor(
@@ -14,43 +18,58 @@ export class LoginUseCase {
         private readonly eventPublisher: IEventPublisher,
         private readonly tokenProvider: ITokenServiceProvider,
         private readonly passwordHasher: IPasswordHasher,
+        private readonly tokenHasher: ITokenHasher,
         private readonly unitOfWork: IUnitOfWork,
-        //Se aplica Unit of work? Porque realmente no se crea nada
-        //Se crea un RefreshToken con una nueva familia
     ) { }
 
     async execute(dto: LoginRequestDTO): Promise<LoginResponseDTO> {
-        //2. Buscar el usuario por su email, normalizado
 
         const emailExists = await this.userRepository.existsByEmail(dto.email);
 
-        //2.1 Si no se encuentra, devolver un error 401. Este error no tiene que diferenciarse
-        //de si la contraseña es incorrecta, para mitigar la enumeración de usuarios
         if(!emailExists){
             throw new InvalidCredentialsError();
         }
 
         const user = await this.userRepository.findByEmail(dto.email);
 
-        //2.2 Si se encuentra, pero la cuenta está SUSPENDED o DELETED, se envía un error 403
-        //sin detalles.
         if(user?.status != "ACTIVE"){
             throw new ForbiddenError();
         }
 
-        //3. Comparar los passwords
-
         const verifiedPassword = await this.passwordHasher.verify(dto.password, user.passwordHash);
 
-         //3.1 Si la contraseña no coinicide, devolver error 401
         if(!verifiedPassword){
             throw new InvalidCredentialsError();
         }
 
-        //4. Generar un RefreshToken y un access Token
+        const userTransformed = User.reconstitute(user);
 
-        //5. Generar una entidad de RefreshToken, generar un familyId, y guardarlo en la BD
+        const tokens = await this.tokenProvider.generate(userTransformed);
 
-        //6. Devolver los datos públicos del usuario y los tokens
+        const hashedToken = await this.tokenHasher.hash(tokens.refreshToken);
+
+        const refreshToken = RefreshToken.create({
+            id: randomUUID(),
+            userId: userTransformed.id,
+            tokenHash: hashedToken,
+            familyId: randomUUID(),
+            expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+            deviceInfo: null,
+        })
+
+        await this.unitOfWork.execute(async (repositories) => {
+            await repositories.refreshToken.save(refreshToken);
+        })
+
+        return {
+            user: {
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role,
+            },
+            accessToken: tokens.accessToken,
+            refreshToken: tokens.refreshToken,
+        }
     }
 }
