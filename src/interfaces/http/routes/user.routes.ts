5c00187a8ee46154d819c8b8e19b61133e8ef873
dodonk4 <ismael.madarieta@hotmail.com>
2026-08-19 14:58:38 -0300

    feat: adding login to user route

--- src/interfaces/http/routes/user.routes.ts
+++ src/interfaces/http/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { UserController } from "../controllers/entities/UserController";
 import { validateRequest } from "../validators/validateRequest";
 import { registerUserSchema } from "../schemas/registerUserSchema";
+import { loginSchema } from "../schemas/loginSchema";
 
 export const createUserRouter = (
   userController: UserController,
@@ -13,6 +14,12 @@ export const createUserRouter = (
     validateRequest(registerUserSchema),
     userController.register,
   );
+
+  router.post(
+    "/login",
+    validateRequest(loginSchema),
+    userController.login,
+  )
 
   return router;
 };
