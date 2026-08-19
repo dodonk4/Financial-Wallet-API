dda6c1fba279999df3b22e9ac77744f69559f601
dodonk4 <ismael.madarieta@hotmail.com>
2026-08-19 14:57:01 -0300

    refactor: deleting comented imports

--- src/infrastructure/events/NodeEventPublisher.ts
+++ src/infrastructure/events/NodeEventPublisher.ts
@@ -2,8 +2,6 @@ import { EventEmitter } from "node:events";
 
 import type { DomainEvent } from "../../domain/events/DomainEvent";
 import { IEventPublisher } from "../../application/ports/output/IEventPublisher";
-// import type { IEventHandler } from "./IEventHandler";
-// import type { IEventPublisher } from "./IEventPublisher";
 
 //I define the handler here. The project doesn't need the complexity of having several handlers types
 type EventHandler<T extends DomainEvent> =
