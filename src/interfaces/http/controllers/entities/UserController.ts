1d2e6359ebe8e3ff385d47057879d5154d58e25d
dodonk4 <ismael.madarieta@hotmail.com>
2026-08-19 14:57:57 -0300

    feat: adding login to UserController

--- src/interfaces/http/controllers/entities/UserController.ts
+++ src/interfaces/http/controllers/entities/UserController.ts
@@ -2,10 +2,13 @@ import { Request, Response, NextFunction } from "express";
 
 import { RegisterUserRequestDTO } from "../../../../application/use-cases/auth/register/RegisterUserRequestDTO.ts";
 import { RegisterUserUseCase } from "../../../../application/use-cases/auth/register/RegisterUserUseCase.ts";
+import { LoginRequestDTO } from "../../../../application/use-cases/auth/login/LoginRequestDTO.ts";
+import { LoginUseCase } from "../../../../application/use-cases/auth/login/LoginUseCase.ts";
 
 export class UserController {
   constructor(
     private readonly registerUserUseCase: RegisterUserUseCase,
+    private readonly loginUseCase: LoginUseCase,
   ) {}
 
   register = async (
@@ -13,12 +16,16 @@ export class UserController {
     res: Response,
     next: NextFunction,
   ): Promise<void> => {
-    try {
       const result = await this.registerUserUseCase.execute(req.body);
-
       res.status(201).json(result);
-    } catch (error) {
-      next(error);
-    }
   };
+
+  login = async (
+    req: Request<unknown, unknown, LoginRequestDTO>,
+    res: Response,
+    next: NextFunction,
+  ): Promise <void> => {
+    const result = await this.loginUseCase.execute(req.body);
+    res.status(201).json(result);
+  }
 }
