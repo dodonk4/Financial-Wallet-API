7a630cf84c67f22b548a96ca21651155e53ec637
dodonk4 <ismael.madarieta@hotmail.com>
2026-08-21 23:07:05 -0300

    feat: adding new methods to IRefreshTokenRepository

--- src/application/ports/output/IRefreshTokenRepository.ts
+++ src/application/ports/output/IRefreshTokenRepository.ts
@@ -2,4 +2,12 @@ import { RefreshToken } from "../../../domain/entities/RefreshToken";
 
 export interface IRefreshTokenRepository {
   save(refreshToken: RefreshToken): Promise<void>;
+
+  consumeById(id: string): Promise<number>;
+
+  findById(id: string): Promise<RefreshToken | null>;
+
+  findByTokenHash(tokenHash: string): Promise<RefreshToken | null>;
+
+  revokeManyByFamilyId(familyId: string): Promise<void>;
 }
