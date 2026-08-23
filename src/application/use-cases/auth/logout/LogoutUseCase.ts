e961d8d28a7fce586b51540e780005bde4cafbee
dodonk4 <ismaelmadarieta49@gmail.com>
2026-08-23 16:41:05 -0300

    feat: adding LogoutUseCase

---
+++ src/application/use-cases/auth/logout/LogoutUseCase.ts
@@ -0,0 +1,32 @@
+import { InvalidCredentialsError } from "../../../../domain/errors/InvalidCredentialsError";
+import { RefreshTokenNotFoundError } from "../../../../domain/errors/RefreshTokenNotFoundError";
+import { ITokenHasher } from "../../../ports/output/ITokenHasher";
+import { IUnitOfWork } from "../../../ports/output/IUnitOfWork";
+import { LogoutRequestDTO } from "./LogoutRequestDTO";
+
+export class LogoutUseCase {
+    constructor(
+        private readonly unitOfWork: IUnitOfWork,
+        private readonly tokenHasher: ITokenHasher,
+    ) { }
+
+    async execute(dto: LogoutRequestDTO) {
+
+        if (!dto.authorization) {
+            throw new InvalidCredentialsError();
+        }
+
+        await this.unitOfWork.execute(async (repositories) => {
+            const tokenHash = await this.tokenHasher.hash(dto.authorization);
+
+            const refreshTokenToRevoke = await repositories.refreshToken.findByTokenHash(tokenHash);
+
+            if (!refreshTokenToRevoke) {
+                throw new RefreshTokenNotFoundError();
+            }
+
+            await repositories.refreshToken.revoke(refreshTokenToRevoke.id);
+
+        })
+    }
+}
