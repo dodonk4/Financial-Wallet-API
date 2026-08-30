902e103d2c9abc1edf7c3a66e1e4991bcd51e29b
dodonk4 <ismaelmadarieta49@gmail.com>
2026-08-30 15:26:09 -0300

    feat: implementing ZodErrorHandler inside validateRequest

--- src/interfaces/http/validators/validateRequest.ts
+++ src/interfaces/http/validators/validateRequest.ts
@@ -3,17 +3,20 @@ import { ZodObject, ZodError } from "zod";
 
 export const validateRequest =
   (schema: ZodObject) =>
-  (req: Request, _res: Response, next: NextFunction): void => {
-    try {
-      req.body = schema.parse(req.body);
+    (req: Request, _res: Response, next: NextFunction): void => {
+      try {
 
-      next();
-    } catch (error) {
-      if (error instanceof ZodError) {
+        req.body = schema.parse(req.body);
+
+        next();
+      } catch (error) {
+        if (error instanceof ZodError) {
+          _res.status(400).json({
+            message: "Validation failed",
+            errors: error.message,
+          });
+          return;
+        }
         next(error);
-        return;
       }
-
-      next(error);
-    }
-  };
+    };
