031066ae79ce6645abd1d70c7528e3e4d8aa8cd2
dodonk4 <ismael.madarieta@hotmail.com>
2026-09-01 21:32:13 -0300

    feat: adding refreshToken creations to seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "node:crypto";
 import { prisma } from "../src/infrastructure/database/prisma"
 
 async function main() {
@@ -6,6 +7,9 @@ async function main() {
         where: { email: "correct.user@mock.com" },
         update: {},
         create: {
+            id: "931cb862-b067-4fed-a4f9-827940e83a9e",
+            //I generate id manually to be able to have a reusable refresh token revoked
+            //If its randomize, I cannot have a refreshToken that is allways the same form the refrshToken tests
             email: "correct.user@mock.com",
             passwordHash: "$argon2id$v=19$m=65536,p=4,t=3$qbceNDFO/vhwkql85U6wKA$pXAzfsNDJVwNUCP7xmVQeFqkuLZhrMIM4aVh0PLQfGM",
             firstName: "Correct User",
@@ -28,7 +32,33 @@ async function main() {
         }
     });
 
-    
+    const revokedToken = await prisma.refreshToken.upsert({
+        where: { tokenHash: "ac4da12bf165dd1d26b59125cf5976ff4a8ca17f7c085f804d97b18f891ec6cb" },
+        update: {},
+        create: {
+            userId: "931cb862-b067-4fed-a4f9-827940e83a9e",
+            tokenHash: "ac4da12bf165dd1d26b59125cf5976ff4a8ca17f7c085f804d97b18f891ec6cb",
+            //Manual revoked tokenHash
+            familyId: randomUUID(),
+            revoked: true,
+            expiresAt: new Date(),
+        }
+    })
+
+    const usedToken = await prisma.refreshToken.upsert({
+        where: { tokenHash: "c1190ffa5aa4037d9578199974073175e25cc6b93fdc46a2e2e276fa29a72942" },
+        update: {},
+        create: {
+            userId: "931cb862-b067-4fed-a4f9-827940e83a9e",
+            tokenHash: "c1190ffa5aa4037d9578199974073175e25cc6b93fdc46a2e2e276fa29a72942",
+            //Manual used tokenHash
+            familyId: randomUUID(),
+            used: true,
+            expiresAt: new Date(),
+        }
+    })
+
+
 }
 
 main().
