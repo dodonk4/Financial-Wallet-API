5828502d309c6d245f0e5598c6eda5528716fd6b
dodonk4 <ismael.madarieta@hotmail.com>
2026-09-06 15:19:50 -0300

    feat: adding method 'saveIdempotencyKey'to IIdempotencyStore

--- src/application/ports/output/IIdempotencyStore.ts
+++ src/application/ports/output/IIdempotencyStore.ts
@@ -1,5 +1,7 @@
 import { Transaction } from "../../../domain/entities/Transaction";
 
 export interface IIdempotencyStore{
+    saveIdempotencyKey(idempotencyKey: string, payload: Transaction): Promise<string>
+
     searchIdempotencyKey(idempotencyKey: string): Promise<Transaction>
 }
