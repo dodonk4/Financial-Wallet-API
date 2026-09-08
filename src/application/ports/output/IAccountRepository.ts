034f28561f88ab3456dd166e076cfea6d679ed17
dodonk4 <ismaelmadarieta49@gmail.com>
2026-09-07 23:57:33 -0300

    feat: adding method findById to AccountRepository

--- src/application/ports/output/IAccountRepository.ts
+++ src/application/ports/output/IAccountRepository.ts
@@ -2,4 +2,6 @@ import { Account } from "../../../domain/entities/Account";
 
 export interface IAccountRepository {
   create(account: Account): Promise<Account>;
+
+  findById(id: string): Promise<Account>;
 }
--- src/infrastructure/persistence/prisma/repositories/PrismaAccountRepository.ts
+++ src/infrastructure/persistence/prisma/repositories/PrismaAccountRepository.ts
@@ -1,6 +1,7 @@
 import { IAccountRepository } from "../../../../application/ports/output/IAccountRepository.ts";
 import { Account } from "../../../../domain/entities/Account.ts";
 import { Prisma, PrismaClient } from "../../../../../generated/prisma/client.ts";
+import { AccountNotFound } from "../../../../domain/errors/AccountNotFoundError.ts";
 
 
 export class PrismaAccountRepository implements IAccountRepository {
@@ -25,5 +26,24 @@ export class PrismaAccountRepository implements IAccountRepository {
     return createdAccount;
   }
 
-  // resto de métodos...
+  async findById(id: string): Promise<Account> {
+    const account = await this.prisma.account.findUnique({ where: { id } });
+
+    if (!account) {
+      throw new AccountNotFound();
+    }
+
+    const response = Account.reconstitute({
+      id: account.id,
+      userId: account.userId,
+      currency: account.currency,
+      balanceCache: Number(account.balanceCache),
+      heldBalance: Number(account.heldBalance),
+      status: account.status,
+      createdAt: account.createdAt,
+      updatedAt: account.updatedAt,
+    });
+
+    return response;
+  }
 }
