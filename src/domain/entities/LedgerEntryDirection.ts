b09b5ee2c5705e79df8486ab703e79f113ab3c76
dodonk4 <ismaelmadarieta49@gmail.com>
2026-09-09 23:28:40 -0300

    fix: commiting deletions

--- src/domain/entities/LedgerEntryDirection.ts
+++
@@ -1,6 +0,0 @@
-export const LedgerEntryDirection = {
-  DEBIT: "DEBIT",
-  CREDIT: "CREDIT",
-};
-
-export type LedgerEntryDirection = (typeof LedgerEntryDirection)[keyof typeof LedgerEntryDirection];
