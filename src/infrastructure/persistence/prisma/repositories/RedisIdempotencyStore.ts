b218e64d72cb63c137e078f89efb9f1ff0c7af4a
dodonk4 <ismaelmadarieta49@gmail.com>
2026-09-09 23:21:05 -0300

    feat: adding hashing to RedisIdempotencyStore

--- src/infrastructure/persistence/prisma/repositories/RedisIdempotencyStore.ts
+++ src/infrastructure/persistence/prisma/repositories/RedisIdempotencyStore.ts
@@ -1,28 +1,28 @@
 import { IIdempotencyStore, TransactionPayload } from "../../../../application/ports/output/IIdempotencyStore";
 import Redis from "ioredis";
+import { ITokenHasher } from "../../../../application/ports/output/ITokenHasher";
 
 export class RedisIdempotencyStore implements IIdempotencyStore {
-  constructor(private readonly redis: Redis) {}
+  constructor(
+    private readonly redis: Redis,
+    private readonly hasherProvider: ITokenHasher
+  ) { }
 
   async saveIdempotencyKey(idempotencyKey: string, paylaod: TransactionPayload): Promise<string> {
 
-    const stringifyPayload = JSON.stringify(paylaod);
+    const hashedPayload = await this.hasherProvider.hash(JSON.stringify(paylaod));
 
-    const result = await this.redis.set(idempotencyKey, stringifyPayload);
+    const result = await this.redis.set("idempotencyKey:" + idempotencyKey, hashedPayload);
 
-    if(!result){
-        throw new Error("An error occured while saving the idempotencyKey");
+    if (!result) {
+      throw new Error("An error occured while saving the idempotencyKey");
     }
 
     return result;
   }
 
-  async searchIdempotencyKey(idempotencyKey: string): Promise<string> {
-    const payload = await this.redis.get(idempotencyKey);
-
-    if (!payload) {
-      throw new Error("There's no transaction found"); //Replace error
-    }
+  async searchIdempotencyKey(idempotencyKey: string): Promise<string | null> {
+    const payload = await this.redis.get("idempotencyKey:" + idempotencyKey);
 
     return payload;
   }
