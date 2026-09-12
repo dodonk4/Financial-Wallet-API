7d8366dc4ed15b18056b2c848a7489814ce61210
dodonk4 <ismaelmadarieta49@gmail.com>
2026-09-12 18:41:59 -0300

    feat: adding transfer instances to compositionRootTest

--- tests/compositionRootTest.ts
+++ tests/compositionRootTest.ts
@@ -22,6 +22,11 @@ import { LogoutUseCase } from "./../src/application/use-cases/auth/logout/Logout
 import { LogoutAllUseCase } from "./../src/application/use-cases/auth/logout/LogoutAllUseCase.ts";
 import { PrismaTestUnitOfWork } from "../src/interfaces/persistence/prisma/repositories/PrismaTestUnitOfWork.ts";
 import errorHandler from "../src/interfaces/http/middlewares/errorHandler.ts";
+import { RedisIdempotencyStore } from "../src/interfaces/persistence/prisma/repositories/RedisIdempotencyStore.ts";
+import { redisClient } from "../src/infrastructure/cache/redisClient.ts";
+import { TransferUsecase } from "../src/application/use-cases/transfers/transfer/TransferUseCase.ts";
+import { TransfersController } from "../src/interfaces/http/controllers/entities/TransfersController.ts";
+import { createTranfersRouter } from "../src/interfaces/http/routes/transfers.routes.ts";
 
 
 const userRepository = new PrismaUserRepository(prisma);
@@ -35,6 +40,8 @@ const tokenHasher = new SHA256Hasher();
 const eventPublisher = new NodeEventPublisher();
 
 const unitOfWork = new PrismaTestUnitOfWork(prisma);
+
+const idempotencyStore = new RedisIdempotencyStore(redisClient, tokenHasher);
 
 const registerUserUseCase = new RegisterUserUseCase(
   userRepository,
@@ -70,6 +77,12 @@ const logoutAllUseCase = new LogoutAllUseCase(
   tokenHasher,
 )
 
+const transferUseCase = new TransferUsecase(
+  idempotencyStore,
+  tokenHasher,
+  unitOfWork,
+)
+
 const authController = new AuthController(
   registerUserUseCase,
   loginUseCase,
@@ -78,11 +91,20 @@ const authController = new AuthController(
   logoutAllUseCase
 );
 
+const transfersController = new TransfersController(
+  transferUseCase,
+)
+
 const authRouter = createAuthRouter(
   authController,
 );
 
+const transfersRouter = createTranfersRouter(
+  transfersController,
+)
+
 app.use("/auth", authRouter);
+app.use("/transfers", transfersRouter);
 app.use("/health", healthRouter);
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Financial Wallet API' });
