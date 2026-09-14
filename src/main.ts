3beac04ea098ac01f3686d001412ab647b99b020
dodonk4 <ismaelmadarieta49@gmail.com>
2026-09-13 22:23:56 -0300

    feat: implementation of httpServer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { createServer } from "node:http";
 import { app } from "./compositionRoot.ts";
 import { logger } from "./infrastructure/logger/index.ts";
 
 const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
+const httpServer = createServer(app);
+
+httpServer.listen(PORT, () => {
   logger.info(`🚀 Server running on port ${PORT}`);
 });
