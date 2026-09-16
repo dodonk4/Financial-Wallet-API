470df661741e07b1624ef5937a9e6d3d7af8499a
dodonk4 <ismael.madarieta@hotmail.com>
2026-09-15 23:19:37 -0300

    refactor: deleting createServer and just importing httpServer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-import { createServer } from "node:http";
-import { app } from "./compositionRoot.ts";
+import { httpServer } from "./compositionRoot.ts";
 import { logger } from "./infrastructure/logger/index.ts";
 
 const PORT = Number(process.env.PORT) || 3000;
-
-const httpServer = createServer(app);
 
 httpServer.listen(PORT, () => {
   logger.info(`🚀 Server running on port ${PORT}`);
