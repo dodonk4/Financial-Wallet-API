921cbf823e6afcbfdd9deb23802216222e5e9fc8
dodonk4 <ismael.madarieta@hotmail.com>
2026-09-19 11:24:17 -0300

    test: completing socket tests

--- tests/unitaries/sockets.test.ts
+++ tests/unitaries/sockets.test.ts
@@ -1,165 +1,66 @@
+import request from "supertest"
 import { io, Socket } from "socket.io-client";
-import { TransferUsecase } from "../../src/application/use-cases/transfers/transfer/TransferUseCase";
-import { RedisIdempotencyStore } from "../../src/interfaces/persistence/redis/RedisIdempotencyStore";
 import { redisClient } from "../../src/infrastructure/cache/redisClient";
-import { SHA256Hasher } from "../../src/infrastructure/security/SHA256Hasher";
-import { prisma } from "../compositionRootTest";
-import { PrismaTransactionRepository } from "../../src/interfaces/persistence/prisma/repositories/PrismaTransactionRepository";
-import { SocketIoNotificationPublisher } from "../../src/interfaces/persistence/socket-io/Socket-ioNotificationPublisher";
-import { createServer } from "node:http";
-import { PrismaTestUnitOfWork } from "../../src/interfaces/persistence/prisma/repositories/PrismaTestUnitOfWork";
-import { Transaction } from "../../src/domain/entities/Transaction";
+import { httpServer as walletApiServer, prisma } from "../compositionRootTest";
 import { AddressInfo } from "node:net";
-import { INotificationPublisher } from "../../src/application/ports/output/INotificationPublisher";
+import { Transaction } from "../../src/domain/entities/Transaction";
 
-describe("", () => {
+describe("Sockets unitary tests", () => {
 
-  let clientSocket: Socket;
-  let clientSocket2: Socket;
+  let clientSocketWallet: Socket;
 
-  const hashProvider = new SHA256Hasher();
-  const idempotencyStore = new RedisIdempotencyStore(redisClient, hashProvider);
-  const unitOfWork = new PrismaTestUnitOfWork(prisma);
-  const transactionRepository = new PrismaTransactionRepository(prisma);
-  let notificationPublisher: INotificationPublisher;
-  // console.log(httpServer.address());
-
-  beforeAll((done) => {
-    const httpServer = createServer();
-    notificationPublisher = new SocketIoNotificationPublisher(httpServer, "f66e84c4-8ab0-4010-8102-3f1a3fd5eb08");
-    // notificationPublisher.connection("f66e84c4-8ab0-4010-8102-3f1a3fd5eb08");
-
-    httpServer.listen(Number(process.env.PORT) || 3000, () => {
-      clientSocket = io(`http://localhost:${(httpServer.address() as AddressInfo).port}`);
-      clientSocket2 = io(`http://localhost:${(httpServer.address() as AddressInfo).port}`);
-      // io.on("connection", (socket: any) => {
-      //   serverSocket = socket;
-      // });
-      //Esto lo hace directamente el notificationPublisher cuando se instancia
-      // console.log("Conectado");
-
-      clientSocket.on("connect", done);
-
+  beforeEach(async () => {
+    redisClient.flushall();
+    await new Promise<void>((resolve) => {
+      walletApiServer.listen(
+        Number(process.env.PORT) || 3000,
+        () => {
+          clientSocketWallet = io(
+            `http://localhost:${(walletApiServer.address() as AddressInfo).port}`
+          );
+          clientSocketWallet.on("connect", () => {
+            clientSocketWallet.emit("createRoom", "f66e84c4-8ab0-4010-8102-3f1a3fd5eb08");
+            clientSocketWallet.once("roomCreated", () => {
+              resolve();
+            });
+          });
+        }
+      );
     });
+  });
+
+  test("Transfer endpoint", async () => {
+    const transactionPromise = new Promise<Transaction>((resolve) => {
+      clientSocketWallet.once("transaction", (transaction) => {
+        resolve(transaction);
+      });
+    });
+
+    const response = await request(walletApiServer)
+      .post("/transfers")
+      .send({
+        originAccountId: "f66e84c4-8ab0-4010-8102-3f1a3fd5eb08",
+        destinyAccountId: "2291d9c5-724f-4c09-a5d2-7dfa8af51f63",
+        amount: 2000,
+        currency: "ARS",
+        description: "Generic description",
+        idempotencyKey: "a6bb88de-9fb5-4a18-b77e-49d2347b791e"
+      });
+
+    const transaction: any = await transactionPromise;
+
+    expect(transaction.props.props.idempotencyKey).toBe("a6bb88de-9fb5-4a18-b77e-49d2347b791e");
 
   });
 
-
-
-  test("should work", (done) => {
-
-    const transactionMock = Transaction.create({
-      id: "ejemplo",
-      type: "TRANSFER",
-      amount: 123,
-      currency: "ARS",
-      idempotencyKey: "safafsaf",
-      relatedTransactionId: "assafas",
-      description: "safafsa",
-    });
-
-    clientSocket.on("transaction", (transaction) => {
-      console.log(transaction);
-      done();
-    });
-
-    notificationPublisher.emitSuccesfulTransaction(transactionMock);
-
-  });
-
-  // it("transaction emit", async () => {
-
-  //   const transactionMock = Transaction.create({
-  //     id: "ejemplo",
-  //     type: "TRANSFER",
-  //     amount: 123,
-  //     currency: "ARS",
-  //     idempotencyKey: "safafsaf",
-  //     relatedTransactionId: "assafas",
-  //     description: "safafsa",
-  //   })
-
-
-  //   // clientSocket.on("hello", () => {
-  //   //   console.log("hello server");
-  //   // });
-
-  //   clientSocket.on("transaction", (algo: string) => {
-  //     console.log("Transaction is: ", algo);
-  //   })
-  //   notificationPublisher.emitSuccesfulTransaction(transactionMock);
-
-
-  //   // await transferUseCase.execute(dataToSend);
-  // });
+  afterEach(() => {
+    clientSocketWallet.disconnect();
+    walletApiServer.closeAllConnections();
+    walletApiServer.close();
+  })
 
   afterAll(async () => {
     await prisma.$disconnect();
     await redisClient.quit();
-    clientSocket.disconnect();
   })
 })
-
-// import { io as ioc, type Socket as ClientSocket } from "socket.io-client";
-// import { Server, type Socket as ServerSocket } from "socket.io";
-// import { INotificationPublisher } from "../../src/application/ports/output/INotificationPublisher";
-
-// function waitFor(socket: ServerSocket | ClientSocket, event: string) {
-//   return new Promise((resolve) => {
-//     socket.once(event, resolve);
-//   });
-// }
-
-// describe("my awesome project", () => {
-//   let io: any, serverSocket: ServerSocket, clientSocket: ClientSocket;
-
-//   beforeAll((done) => {
-//     const httpServer = createServer();
-//     io = new Server(httpServer);
-//     httpServer.listen(() => {
-//       const port = (httpServer.address() as AddressInfo).port;
-//       clientSocket = ioc(`http://localhost:${port}`);
-//       io.on("connection", (socket: any) => {
-//         serverSocket = socket;
-//       });
-//       clientSocket.on("connect", done);
-//     });
-//   });
-
-//   afterAll(() => {
-//     io.close();
-//     clientSocket.disconnect();
-//   });
-
-//   test("should work", (done) => {
-//     clientSocket.on("hello", (arg) => {
-//       expect(arg).toBe("world");
-//       done();
-//     });
-//     serverSocket.emit("hello", "world");
-//   });
-
-//   test("should work with an acknowledgement", (done) => {
-//     serverSocket.on("hi", (cb) => {
-//       cb("hola");
-//     });
-//     clientSocket.emit("hi", (arg: any) => {
-//       expect(arg).toBe("hola");
-//       done();
-//     });
-//   });
-
-//   test("should work with emitWithAck()", async () => {
-//     serverSocket.on("foo", (cb) => {
-//       cb("bar");
-//     });
-//     const result = await clientSocket.emitWithAck("foo");
-//     expect(result).toBe("bar");
-//   });
-
-//   test("should work with waitFor()", () => {
-//     clientSocket.emit("baz");
-
-//     return waitFor(serverSocket, "baz");
-//   });
-// });
