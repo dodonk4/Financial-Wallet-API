5bb5f099f20837a1900a9746a19adcd50bbde5f9
dodonk4 <ismael.madarieta@hotmail.com>
2026-09-19 11:27:44 -0300

    feat: implementing socket rooms

--- src/interfaces/persistence/socket-io/Socket-ioNotificationPublisher.ts
+++ src/interfaces/persistence/socket-io/Socket-ioNotificationPublisher.ts
@@ -2,37 +2,33 @@ import { Server as httpServer } from "node:http";
 import { Server as ioServer } from "socket.io";
 import { Transaction } from "../../../domain/entities/Transaction";
 import { INotificationPublisher } from "../../../application/ports/output/INotificationPublisher";
+import { IUserRepository } from "../../../application/ports/output/IUserRepository";
 
 export class SocketIoNotificationPublisher implements INotificationPublisher {
 
     io: ioServer;
-    room: string | undefined;
-    constructor(private httpServer: httpServer, private userId: string) {
+    constructor(
+        private httpServer: httpServer,
+    ) {
         this.io = new ioServer(this.httpServer);
         this.io.on("connection", (socket) => {
-            // this.room = `user:${this.userId}`
-            // socket.join(this.room);
-            // this.io.to(this.room).emit("hello");
-            // console.log("This room was established as: ", this.room);
-            console.log("Socket connected: Id: ", socket.id);
+            socket.on("createRoom", (userId: string) => {
+                const room = `user:${userId}`;
+
+                socket.join(room);
+
+                this.io.to(room).emit("roomCreated");
+            });
         });
-        this.io.emit("hello");
+
+
     }
 
-
-    // connection(userId: string): void {
-    //     // this.io.on("connection", (socket) => {
-    //     //     this.room = `user:${userId}`
-    //     //     socket.join(this.room);
-    //     //     console.log("This room was established as: ", this.room);
-    //     //     console.log("Socket connected: Id: ", socket.id);
-    //     // })
-    // }
-
-    emitSuccesfulTransaction(transaction: Transaction): void {
-        this.io.emit("transaction", transaction);
-
-
+    disconnectClient(userId: string) {
+        this.io.in(`user:${userId}`).disconnectSockets(true);
+    }
+    emitSuccesfulTransaction(transaction: Transaction, userId: string): void {
+        this.io.to(`user:${userId}`).emit("transaction", transaction);
     }
 
 
