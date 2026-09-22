df80bd491f4ffc75b1a444d0d613ef93cf47ef15
dodonk4 <ismael.madarieta@hotmail.com>
2026-09-21 23:47:30 -0300

    feat: implementing cacheFlush and transferAuth to tests

--- tests/integration/transfer.test.ts
+++ tests/integration/transfer.test.ts
@@ -1,12 +1,46 @@
 import request from "supertest"
 import { httpServer, prisma } from "../compositionRootTest"
 import { redisClient } from "../../src/infrastructure/cache/redisClient";
+import { SHA256Hasher } from "../../src/infrastructure/security/SHA256Hasher";
+import { RedisIdempotencyStore } from "../../src/interfaces/persistence/redis/RedisIdempotencyStore";
 
 describe("Transfers endpoints", () => {
     it("should return 201 and the transaction done", async () => {
-        
+
+        const login = await request(httpServer)
+            .post("/auth/login")
+            .send({
+                email: "correct.user@mock.com",
+                password: "abcd1234",
+            });
+
         const response = await request(httpServer)
             .post("/transfers")
+            .set("Authorization", `Bearer ${login.body.accessToken}`)
+            .send({
+                "originAccountId": "2291d9c5-724f-4c09-a5d2-7dfa8af51f63",
+                "destinyAccountId": "f66e84c4-8ab0-4010-8102-3f1a3fd5eb08",
+                "amount": 2000,
+                "currency": "ARS",
+                "description": "Generic description",
+                "idempotencyKey": "a6bb77de-9fb5-4a20-b77e-49d2347b791e"
+            });
+
+        expect(response.status).toBe(201);
+    });
+
+    it("should return 403 when a user tries to send a transfer from an account that belongs to another user", async () => {
+
+        const login = await request(httpServer)
+            .post("/auth/login")
+            .send({
+                email: "correct.user@mock.com",
+                password: "abcd1234",
+            });
+
+        const response = await request(httpServer)
+            .post("/transfers")
+            .set("Authorization", `Bearer ${login.body.accessToken}`)
             .send({
                 "originAccountId": "f66e84c4-8ab0-4010-8102-3f1a3fd5eb08",
                 "destinyAccountId": "2291d9c5-724f-4c09-a5d2-7dfa8af51f63",
@@ -16,8 +50,38 @@ describe("Transfers endpoints", () => {
                 "idempotencyKey": "a6bb77de-9fb5-4a20-b77e-49d2347b791e"
             });
 
-        expect(response.status).toBe(201);
+        expect(response.status).toBe(403);
     });
+
+    it("should return 400 as a zod error when a user tries to send a transfer with the same value in originAccountId and destinyAccountId", async () => {
+
+        const login = await request(httpServer)
+            .post("/auth/login")
+            .send({
+                email: "correct.user@mock.com",
+                password: "abcd1234",
+            });
+
+        const response = await request(httpServer)
+            .post("/transfers")
+            .set("Authorization", `Bearer ${login.body.accessToken}`)
+            .send({
+                "originAccountId": "2291d9c5-724f-4c09-a5d2-7dfa8af51f63",
+                "destinyAccountId": "2291d9c5-724f-4c09-a5d2-7dfa8af51f63",
+                "amount": 2000,
+                "currency": "ARS",
+                "description": "Generic description",
+                "idempotencyKey": "a6bb77de-9fb5-4a20-b77e-49d2347b791e"
+            });
+
+        expect(response.status).toBe(400);
+    });
+
+    afterEach(async () => {
+        const hasherProvider = new SHA256Hasher();
+        const cache = new RedisIdempotencyStore(redisClient, hasherProvider);
+        await cache.flushAll();
+    })
 
     afterAll(async () => {
         await prisma.$disconnect();
--- tests/unitaries/sockets.test.ts
+++ tests/unitaries/sockets.test.ts
@@ -4,6 +4,8 @@ import { redisClient } from "../../src/infrastructure/cache/redisClient";
 import { httpServer as walletApiServer, prisma } from "../compositionRootTest";
 import { AddressInfo } from "node:net";
 import { Transaction } from "../../src/domain/entities/Transaction";
+import { RedisIdempotencyStore } from "../../src/interfaces/persistence/redis/RedisIdempotencyStore";
+import { SHA256Hasher } from "../../src/infrastructure/security/SHA256Hasher";
 
 describe("Sockets unitary tests", () => {
 
@@ -36,8 +38,16 @@ describe("Sockets unitary tests", () => {
       });
     });
 
+    const login = await request(walletApiServer)
+      .post("/auth/login")
+      .send({
+        email: "email.registered@mock.com",
+        password: "abcd1234",
+      });
+
     const __response = await request(walletApiServer)
       .post("/transfers")
+      .set("Authorization", `Bearer ${login.body.accessToken}`)
       .send({
         originAccountId: "f66e84c4-8ab0-4010-8102-3f1a3fd5eb08",
         destinyAccountId: "2291d9c5-724f-4c09-a5d2-7dfa8af51f63",
@@ -46,14 +56,19 @@ describe("Sockets unitary tests", () => {
         description: "Generic description",
         idempotencyKey: "a6bb88de-9fb5-4a18-b77e-49d2347b791e"
       });
-
+    
+    
     const transaction: any = await transactionPromise;
 
     expect(transaction.props.props.idempotencyKey).toBe("a6bb88de-9fb5-4a18-b77e-49d2347b791e");
 
   });
 
-  afterEach(() => {
+  afterEach(async () => {
+    const hasherProvider = new SHA256Hasher();
+    const cache = new RedisIdempotencyStore(redisClient, hasherProvider);
+    await cache.flushAll();
+    
     clientSocketWallet.disconnect();
     walletApiServer.closeAllConnections();
     walletApiServer.close();
