583bedd733adc345e7ea3a161e6f6adb43d04e79
dodonk4 <ismael.madarieta@hotmail.com>
2026-09-23 14:19:00 -0300

    feat: adding CheckAccountBalance

---
+++ src/application/use-cases/accounts/checkAccountBalance/CheckAccountBalanceUseCase.ts
@@ -0,0 +1,31 @@
+import { ForbiddenError } from "../../../../domain/errors/ForbiddenError";
+import extractToken from "../../../../interfaces/http/auth/extractToken";
+import { IAccountRepository } from "../../../ports/output/IAccountRepository";
+import { ITokenServiceProvider } from "../../../ports/output/ITokenServiceProvider";
+import { CheckAccountBalanceDTORequest } from "./CheckAccountBalanceDTORequest";
+
+export class CheckAccountBalance {
+    private constructor(
+        private readonly accountRepository: IAccountRepository,
+        private readonly tokenServiceProvider: ITokenServiceProvider) { }
+
+    async execute(dto: CheckAccountBalanceDTORequest, authHeader: string) {
+
+        const token = extractToken(authHeader);
+
+        const decoded = await this.tokenServiceProvider.decodeToken(token);
+
+        const account = await this.accountRepository.findById(dto.accountId);
+
+        if (account.userId != decoded?.sub) {
+            throw new ForbiddenError();
+        }
+
+        const balanceCache = account.balanceCache;
+
+        return {
+            balanceCache
+        }
+
+    }
+}
