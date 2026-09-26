6af24c180013fe3729fa4fcd4beb4db7849f5cad
dodonk4 <ismael.madarieta@hotmail.com>
2026-09-26 13:10:19 -0300

    refactor: changing checkAccountBalanceUseCase DTOs names

--- src/application/ports/input/IAuthMiddleware.ts
+++ src/application/ports/input/IAuthMiddleware.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import type { ParamsDictionary } from 'express-serve-static-core';
-import { CheckAccountBalanceDTORequest } from "../../use-cases/accounts/checkAccountBalance/CheckAccountBalanceDTORequest";
+import { CheckAccountBalanceRequestDTO } from "../../use-cases/accounts/checkAccountBalance/CheckAccountBalanceRequestDTO";
 type RequestVariation = ParamsDictionary |
-CheckAccountBalanceDTORequest;
+CheckAccountBalanceRequestDTO;
 
 export interface IAuthMiddleware {
     verifyAuth(req: Request<RequestVariation>, res: Response, next: NextFunction): Promise<void>
--- src/application/use-cases/accounts/checkAccountBalance/CheckAccountBalanceUseCase.ts
+++ src/application/use-cases/accounts/checkAccountBalance/CheckAccountBalanceUseCase.ts
@@ -2,14 +2,15 @@ import { ForbiddenError } from "../../../../domain/errors/ForbiddenError";
 import extractToken from "../../../../interfaces/http/auth/extractToken";
 import { IAccountRepository } from "../../../ports/output/IAccountRepository";
 import { ITokenServiceProvider } from "../../../ports/output/ITokenServiceProvider";
-import { CheckAccountBalanceDTORequest } from "./CheckAccountBalanceDTORequest";
+import { CheckAccountBalanceRequestDTO } from "./CheckAccountBalanceRequestDTO";
+import { CheckAccountBalanceResponseDTO } from "./CheckAccountBalanceResponseDTO";
 
 export class CheckAccountBalanceUseCase {
     constructor(
         private readonly accountRepository: IAccountRepository,
         private readonly tokenServiceProvider: ITokenServiceProvider) { }
 
-    async execute(dto: CheckAccountBalanceDTORequest, authHeader: string) {
+    async execute(dto: CheckAccountBalanceRequestDTO, authHeader: string): Promise<CheckAccountBalanceResponseDTO> {
 
         const token = extractToken(authHeader);
 
